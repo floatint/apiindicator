bfbf1de0ce8d6b4f0bd6432952b63ce8be66361d
Klishin Ilya <klishin-ilya@mail.ru>
2020-05-22 17:31:35 +0300

    Reports controller fix

--- apiindserver/Controllers/ReportsController.cs
+++ apiindserver/Controllers/ReportsController.cs
@@ -22,20 +22,20 @@ namespace apiindserver.Controllers
         [HttpGet("{id:long}")]
         public async Task<IActionResult> GetProjectDifference(long id, [FromBody] string version)
         {
-            var project = DbContext.Projects.FirstOrDefault(x => x.Id == id);
+            var project = await DbContext.Projects.FirstOrDefaultAsync(x => x.Id == id);
             if (project == null)
                 return StatusCode(StatusCodes.Status404NotFound, id);
             //get avg recponse time for current project version
             var currVerAvgRespTime = DbContext.LogRecords
                 .Include(x => x.Project)
                 .Where(x => x.Project == project)
-                .Where(x => x.Version.Name == version)
+                .Where(x => x.Version == version)
                 .Average(x => x.ResponseTime.Milliseconds);
             //get avg response time for other version
             var otherVerAvgRespTime = DbContext.LogRecords
                 .Include(x => x.Project)
                 .Where(x => x.Project == project)
-                .Where(x => x.Version.Name != version)
+                .Where(x => x.Version != version)
                 .Average(x => x.ResponseTime.Milliseconds);
 
             //TODO: handle criteria
